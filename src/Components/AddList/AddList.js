8fc58de6bc15b30dd77e9a4ee55e67bb1a87d4cb
Evan Perriello <evan.perriello@gmail.com>
2018-04-20 19:44:05 -0400

    Configure input in AddList Component.

--- src/Components/AddList/AddList.js
+++ src/Components/AddList/AddList.js
@@ -5,17 +5,26 @@ class AddList extends Component{
     constructor(props){
         super(props);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.state = {
+            title: ''
+        };
     }
     handleFormSubmit(e){
         e.preventDefault();
-        this.props.submitNewList('Chicken Listin');
+        this.props.submitNewList(this.state.title);
+    }
+    handleInputChange(e){
+        this.setState(
+            {title: e.target.value}
+        )
     }
     render(){
         return (
             <div className="add-list">
                 <h3 className='list-title'>Add a New List</h3>
                 <form onSubmit={this.handleFormSubmit} className="add-list__form">
-                    <input className="add-list__input" type="text" placeholder="New list title"></input>
+                    <input className="add-list__input" type="text" placeholder="New list title" value={this.state.title} onChange={this.handleInputChange}></input>
                     <button className="add-list__button" type="submit">+</button>
                 </form>
             </div>
